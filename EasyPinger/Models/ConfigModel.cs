6323eceda04f16edfa7d9b3354e33b786b4f8de1
nikita.bykov <ironnikit@gmail.com>
2026-06-02 14:02:12 +0300

    added ConfigModel

---
+++ EasyPinger/Models/ConfigModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json; 
+
+namespace EasyPinger.Models
+{
+    internal class ConfigModel
+    {
+        public Service[] Services;
+        public int TimeOut;
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+
+        public void Generate()
+        {
+            this.TimeOut = 3600;
+            this.Services = new Service[1];
+            this.Services[0].DisplayName = "Яндекс";
+            this.Services[0].InternetAddress = "ya.ru";
+        }
+    }
+
+    public class Service
+    {
+        public Service (string ip, string name, int tries)
+        {
+            InternetAddress = ip;
+            DisplayName = name;
+            TriesCounter = tries;
+        }
+
+        public string InternetAddress { get; set; }
+        public string DisplayName { get; set; }
+        public int TriesCounter { get; set; }
+    }
+}
